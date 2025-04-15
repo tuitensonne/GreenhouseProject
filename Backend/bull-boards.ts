0ccb987383c9239957759445298352de788cb771
son.tran22 <son.trannam.bku@gmail.com>
2025-04-15 21:41:29 +0700

    Adding bull board

--- Backend/bull-boards.ts
+++ Backend/bull-boards.ts
@@ -3,8 +3,10 @@ const { createBullBoard } = require('@bull-board/api');
 const { BullAdapter } = require('@bull-board/api/bullAdapter');
 const { ExpressAdapter } = require('@bull-board/express');
 const Queue = require('bull');
-
+const dotenv = require('dotenv')
 const myQueue = new Queue('scheduler', { redis: { host: 'localhost', port: 6060 } });
+
+dotenv.config()
 
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath('/admin/queues');
@@ -17,7 +19,6 @@ createBullBoard({
 const app = express();
 app.use('/admin/queues', serverAdapter.getRouter());
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Bull Board dashboard chạy tại http://localhost:${PORT}/admin/queues`);
+app.listen(process.env.BULL_BOARD_PORT, () => {
+  console.log(`Bull Board dashboard runs at http://${process.env.BULL_BOARD_HOST}:${process.env.BULL_BOARD_PORT}/admin/queues`);
 });
